a45578ebde32b7f02dd9458265a68bcb9b1aaa6c
luizfelipe17 <luizfeliperabelo17@gmail.com>
2024-03-04 23:01:43 -0300

    Add files via upload

---
+++ TerceiraQuestao.java
@@ -0,0 +1,22 @@
+package com.mycompany.primeiraquestao;
+import java.util.Scanner;
+
+public class TerceiraQuestao {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+        
+        String nome;
+        
+        System.out.print("Informe seu nome completo: ");
+            nome = ler.nextLine();
+      
+       System.out.println("Olá, "+nome);
+       System.out.println("Seja bem vindo!!!");
+       System.out.println("Espero que esteja bem ;)");
+    
+    }
+}
+    
+    
+
