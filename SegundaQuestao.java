dbd69846f967f144363f040379e6ff39d880f48b
luizfelipe17 <luizfeliperabelo17@gmail.com>
2024-03-04 22:56:56 -0300

    Add files via upload

---
+++ SegundaQuestao.java
@@ -0,0 +1,25 @@
+package com.mycompany.primeiraquestao;
+import java.util.Scanner;
+
+public class SegundaQuestao {
+
+   
+    public static void main(String[] args) {
+         double numero1, numero2, resto;
+       
+       Scanner ler = new Scanner (System.in);
+       
+       System.out.print("Informe o primeiro número: ");
+            numero1 = ler.nextDouble();
+       
+       System.out.print("Informe o segundo número: ");
+            numero2 = ler.nextDouble();   
+            
+       resto = numero1 % numero2;
+       
+       System.out.println("O resto da divisão será: " +resto);
+   }
+    
+}
+    
+    
