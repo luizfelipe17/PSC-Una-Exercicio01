6a1296901f399c221133bece695db13e9736f69e
luizfelipe17 <luizfeliperabelo17@gmail.com>
2024-03-06 23:32:11 -0300

    Add files via upload

---
+++ OitavaQuestao.java
@@ -0,0 +1,21 @@
+package com.mycompany.exercicio01;
+import java.util.Scanner;
+
+public class OitavaQuestao {
+
+    public static void main(String[] args) {
+        
+        Scanner ler = new Scanner (System.in);
+        
+        double metros, centimetros;
+       
+        System.out.print("Informe o valor em metros: ");
+        
+            metros = ler.nextDouble();
+            
+            centimetros = metros*100;
+            
+        System.out.print("O valor em centímetros será: "+centimetros+"cm");
+    }
+  
+}
