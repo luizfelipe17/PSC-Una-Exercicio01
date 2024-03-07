4b2b4a31b48a5d074b3b4027fa78f445b6b2b921
luizfelipe17 <luizfeliperabelo17@gmail.com>
2024-03-06 22:46:59 -0300

    Add files via upload

---
+++ QuintaQuestao.java
@@ -0,0 +1,23 @@
+package com.mycompany.exercicio01;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class QuintaQuestao {
+
+    public static void main(String[] args) {
+        
+        Scanner ler = new Scanner (System.in);
+        DecimalFormat df = new DecimalFormat("###,##0.00");
+     
+        double valorReal, valorDolar;
+        
+        System.out.print("Digite o valor em 'Dólar': US$");
+            valorDolar = ler.nextDouble();
+            
+            
+            valorReal = valorDolar*4.95;
+            
+            System.out.print("O valor em real será: R$"+(df.format(valorReal)));
+    }
+    
+}
