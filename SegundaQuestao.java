2fe443d5bb5bc0a0e1bae819690f1141424022cc
luizfelipe17 <luizfeliperabelo17@gmail.com>
2024-03-07 21:18:01 -0300

    Update SegundaQuestao.java

--- SegundaQuestao.java
+++ SegundaQuestao.java
@@ -1,6 +1,8 @@
+package com.mycompany.exercicio01;
 import java.util.Scanner;
 
 public class SegundaQuestao {
+
    
     public static void main(String[] args) {
          double numero1, numero2, resto;
@@ -12,10 +14,19 @@ public class SegundaQuestao {
        
        System.out.print("Informe o segundo número: ");
             numero2 = ler.nextDouble();   
+                
+                while(numero2 == 0){
+        
+        System.out.print("Informe um número diferente de zero: ");
+            numero2 = ler.nextDouble(); 
+            
+    }
             
        resto = numero1 % numero2;
        
        System.out.println("O resto da divisão será: " +resto);
+       
+       
    }
     
 }
